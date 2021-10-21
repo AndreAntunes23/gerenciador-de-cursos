e26ebf72f814a202230f4cbb5f61df369952ad59
AndreAntunes23 <andre_lfa@outlook.com>
2021-10-21 10:45:42 -0300

    Aula sobre Maps

--- src/br/com/alura/Curso.java
+++ src/br/com/alura/Curso.java
@@ -1,23 +1,27 @@
 package br.com.alura;
 
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Set;
 
 public class Curso {
-
-	public Curso(String nome, String instrutor) {
-		super();
-		this.nome = nome;
-		this.instrutor = instrutor;
-	}
 
 	private String nome;
 	private String instrutor;
 	private List<Aula> aulas = new LinkedList<Aula>();
 	private Set<Aluno> alunos = new HashSet<>();
+	private Map<Integer, Aluno> matriculaParaAluno = new HashMap<>();
+	
+	public Curso(String nome, String instrutor) {
+		super();
+		this.nome = nome;
+		this.instrutor = instrutor;
+	}
 	
 	public String getNome() {
 		return nome;
@@ -46,6 +50,7 @@ public class Curso {
 
 	public void matricula(Aluno aluno) {
 		this.alunos.add(aluno);
+		this.matriculaParaAluno.put(aluno.getNumeroMatricula(), aluno);
 	}
 	
 	public Set<Aluno> getAlunos() {
@@ -54,6 +59,12 @@ public class Curso {
 
 	public boolean estaMatriculado(Aluno aluno) {
 		return this.alunos.contains(aluno);
+	}
+
+	public Aluno buscaMatriculado(int numero) {
+		if(!matriculaParaAluno.containsKey(numero))
+			throw new NoSuchElementException();
+		return matriculaParaAluno.get(numero);
 	}
 
 }
